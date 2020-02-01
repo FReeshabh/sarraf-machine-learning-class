bf43bc11bc9d76c9d31e0ca40b8fc6e17c23ec41
FReeshabh <rishabhtewari1@gmail.com>
2020-01-31 20:59:18 -0600

    THIS IS IT 	modified:   project1.py 	../Linear_Algebra/

--- project_1/project1.py
+++ project_1/project1.py
@@ -25,21 +25,22 @@ t_target = np.reshape(t_target.values, (-1, 1))
 X = np.hstack((X, np.ones(X.shape)))
 X_norm = np.hstack((X_norm, np.ones(X_norm.shape)))
 
-
+# print(t_target)
 def closed_form_solution(x, t):
     """
     Analytical Solution for the weight for Linear Regression
     """
     weight = (np.linalg.pinv(x) @ t).T #the transpose of the final weight
-    prediction = weight * x
-    print(weight)
+    d_closed = np.linspace(1500, 5500, 3500)
+    print(weight.T)
+    prediction_closed = weight.T [0][0]*d_closed + weight.T[1][0]
     plt.title("Carbig Dataset, Closed Form\n Rishabh Tewari")
     plt.scatter(X[:,0], t, label="actual data")
     plt.xlabel('Weight')
     plt.ylabel('Horsepower')
-    plt.plot(X, prediction, color = "red", label="Closed Form Solution")
+    plt.plot(d_closed, prediction_closed, color = "red", label="Closed Form Solution")
     plt.legend()
-    plt.show()
+    plt.figure() 
 
 
 def gradient_descent(iterations, rho, weight, x, t):
@@ -50,13 +51,15 @@ def gradient_descent(iterations, rho, weight, x, t):
         gradient = ((2* weight.T @ x.T@ x ) - (2* t.T @ x)).T
         weight = weight - (rho * gradient)
         print("Iteration {} 's loss: {}".format(i, (np.square(np.linalg.norm(t - x @ weight)))))
+    d_grad = np.linspace(1500, 5500, 3500)
+    weight[0][0] = weight[0][0] / X.max()
     # print(weight)
-    prediction = weight.T * x
+    prediction_grad = weight.T [0][0]*d_grad + weight.T[0][1]
     plt.title("Carbig Dataset, Gradient Descent\n Rishabh Tewari")
     plt.scatter(X[:,0], t, label="actual data")
     plt.xlabel('Weight')
     plt.ylabel('Horsepower')
-    plt.plot(X, prediction, color = "red", label="Gradient Descent Solution")
+    plt.plot(d_grad, prediction_grad, color = "green", label="Gradient Descent Solution")
     plt.legend()
     plt.show()
 
@@ -71,7 +74,7 @@ rho_learning_rate = 0.001
 
 # Run these functions
 if __name__ == '__main__':
-    Thread(target = closed_form_solution(X_norm, t_target)).start()
+    Thread(target = closed_form_solution(X, t_target)).start()
     Thread(target = gradient_descent(max_iterations, rho_learning_rate, init_weight, X_norm, t_target))
 
 
