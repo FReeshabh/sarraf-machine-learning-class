9bcb0eb676776afb4adffa75eb351103cc043ae1
Rishabh Tewari <rishabhtewari1@gmail.com>
2020-04-21 22:53:54 -0500

    modified:   Project5.py 	deleted:    XOR_ONLINE.py 	modified:   ../project_3/Implementation_Project_3.py 	modified:   ../python_tutorial/class_example.py

--- project_5/Project5.py
+++ project_5/Project5.py
@@ -1,40 +1,46 @@
 import numpy as np 
+import matplotlib.pyplot as plt 
 np.random.seed(42)
 
-C_INPUTS         = np.array([[-1,-1],[-1,1],[1,-1],[1,1]])
-C_TARGET_OUTPUTS = np.array([[-1],[1],[1],[-1]])
+# # PART 1 - 2 layered Neural Network with 2 hidden Units
+INPUT_UNITS, HIDDEN_UNITS, OUTPUT_UNITS = 2, 2, 1
+C_INPUT  = np.array([[0, 0], [0, 1],
+                       [1, 0], [1,1]])
+# 
+C_OUTPUT  = np.array([[0],[1],[1],[0]])
+EPOCHS = 10000
+RHO_LEARNING_RATE = 0.1
 
-# Hyperparameter Selection 
-EPOCHS = 2500
-RHO_LEARNING_RATE = 0.2
+sigmoid_activation = lambda z: (1 / (1 + np.exp(-z)))
+sigmoid_backpropagation = lambda z: (z * (1 - z))
 
-INPUT_UNITS  = 2
-HIDDEN_UNITS = 2 #Only one hidden layer, with two units inside it
-OUTPUT_UNITS = 1
+weight_hidden     = np.random.uniform(size = (INPUT_UNITS, HIDDEN_UNITS)) # 2, 2
+weight_prediction = np.random.uniform(size = (HIDDEN_UNITS, OUTPUT_UNITS)) # 2, 1
+bias_hidden       = np.random.uniform(size = (1, HIDDEN_UNITS)) # 1, 2
+bias_prediction   = np.random.uniform(size = (1, OUTPUT_UNITS)) # 1, 1
 
-# Weight Initialization 2 layered network
-weight_hidden_1 = np.random.normal(size=(INPUT_UNITS, HIDDEN_UNITS)) #(2, 2)
-weight_output_2 = np.random.normal(size=(HIDDEN_UNITS, OUTPUT_UNITS)) #(2, 1)
 
-# Bias Initialization
-bias_hidden_1   = np.random.normal(size=(1, HIDDEN_UNITS)) #(1, 2)
-bias_output_2   = np.random.normal(size=(1, OUTPUT_UNITS)) #(1, 1)
-
-sigmoid_activation = lambda a: (1 / (1 + np.exp(-a)))
-sigmoid_backprop   = lambda a: (a * (1 - a))
-error = lambda prediction, target: (1/2)*(np.square(np.linalg.norm(target - prediction)))
-# error = lambda prediction, target: 2*(prediction - target)
 for i in range(EPOCHS):
-    # Forward pass
-    hidden_layer_output_1 = sigmoid_activation((np.dot(C_INPUTS, weight_hidden_1) + bias_hidden_1)) # (4,2)
-    output_layer_output_2 = sigmoid_activation((np.dot(hidden_layer_output_1, weight_output_2) + bias_output_2)) # (4,1)
+    """Perform forward propagation"""
+    hidden_layer     = sigmoid_activation(np.dot(C_INPUT, weight_hidden) + bias_hidden)
+    # print(*hidden_layer)
+    prediction_layer = sigmoid_activation(np.dot(hidden_layer, weight_prediction))
+    # print(*prediction_layer)
 
-    # Backward Propagation
-    cost_error = error(output_layer_output_2, C_TARGET_OUTPUTS)
-    grad_weight_hidden_1 = np.dot(C_INPUTS.T, (np.dot(cost_error * sigmoid_backprop(C_TARGET_OUTPUTS), weight_output_2.T) * sigmoid_backprop(hidden_layer_output_1))) * RHO_LEARNING_RATE
-    grad_weight_output_2 = (np.dot(hidden_layer_output_1.T, (cost_error * sigmoid_activation(output_layer_output_2))))  * RHO_LEARNING_RATE
+    """Backpropagation"""
+    error_prediction_layer = C_OUTPUT - prediction_layer
+    error_hidden_layer = np.dot((error_prediction_layer * sigmoid_backpropagation(prediction_layer)), weight_prediction.T)
 
-    weight_hidden_1 += grad_weight_hidden_1
-    weight_output_2 += grad_weight_output_2
+    # Calculating gradients for the update
+    grad_prediction_weight = np.dot(hidden_layer.T, (error_prediction_layer * sigmoid_backpropagation(prediction_layer)))
+    grad_prediction_bias   = np.sum(error_prediction_layer * sigmoid_backpropagation(prediction_layer), keepdims=True)
+    grad_hidden_weight     = np.dot(C_INPUT.T, (error_hidden_layer * sigmoid_backpropagation(hidden_layer)))
+    grad_hidden_bias       = np.sum((error_hidden_layer * sigmoid_backpropagation(hidden_layer)), keepdims=True)
 
-    print(cost_error)
+    #Updating Weights and Biases
+    weight_prediction += RHO_LEARNING_RATE * grad_prediction_weight
+    bias_prediction   += RHO_LEARNING_RATE * grad_prediction_bias
+    weight_hidden     += RHO_LEARNING_RATE * grad_hidden_weight
+    bias_hidden       += RHO_LEARNING_RATE * grad_hidden_bias
+
+print(*prediction_layer)
